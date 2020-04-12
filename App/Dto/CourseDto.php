ebb9c7f88db24d63d1c3614226afd321620141a2
Nawwar Elnarsh <sirnarsh@gmail.com>
2020-04-12 16:11:43 +0200

    Detect empty responses/false tokens and break the loop

--- App/Dto/CourseDto.php
+++ App/Dto/CourseDto.php
@@ -4,14 +4,46 @@ namespace App\Dto;
 
 class CourseDto
 {
+    /**
+     * @var int
+     */
     public $id;
+
+    /**
+     * @var string
+     */
     public $title;
+
+    /**
+     * @var string
+     */
     public $url;
+
+    /**
+     * @var int
+     */
     public $completionRatio;
+
+    /**
+     * @var string
+     */
     public $lastAccessedTime;
+
+    /**
+     * @var string
+     */
     public $enrollmentTime;
+
+    /**
+     * @var int
+     */
     public $numberOfCollections;
 
+    /**
+     * CourseDto constructor.
+     * Normalize course object reading selected props
+     * @param array $course
+     */
     public function __construct(array $course)
     {
         $this->id = $course['id'];
--- App/Dto/UdemyResponseDto.php
+++ App/Dto/UdemyResponseDto.php
@@ -12,6 +12,11 @@ class UdemyResponseDto
     public $count = 0;
 
     /**
+     * @var bool
+     */
+    public $lastResponseFailed = false;
+
+    /**
      * @var CourseDto[]
      */
     public $coursesDto = [];
@@ -23,16 +28,27 @@ class UdemyResponseDto
     {
         echo "Appending courses to list object..." . PHP_EOL;
         $this->count = $response['count'];
-        foreach ($response['results'] as $course) {
-            $this->coursesDto[] = new CourseDto($course);
+
+        if (isset($response['results'])) {
+            foreach ($response['results'] as $course) {
+                $this->coursesDto[] = new CourseDto($course);
+            }
+        } else {
+            echo "Response didn't contain courses list " . PHP_EOL;
+            $this->lastResponseFailed = true;
         }
     }
 
     /**
+     * isFinished bool by comparing done vs total courses count
      * @return bool
      */
     public function isFinished()
     {
+        if ($this->lastResponseFailed) {
+            return true;
+        }
+
         $done = count($this->coursesDto);
         echo "Got {$done} of {$this->count} courses" . PHP_EOL;
         return $this->count === count($this->coursesDto);
